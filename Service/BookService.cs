920760ec2929f42116768c7d5d8151737a80c8a8
Stuvian <hp40402@gmail.com>
2025-04-09 13:55:41 +0800

    service

---
+++ Service/BookService.cs
@@ -0,0 +1,41 @@
+﻿using OnlineBookClub.DTO;
+using OnlineBookClub.Models;
+using OnlineBookClub.Repository;
+
+namespace OnlineBookClub.Service
+{
+    public class BookService
+    {
+        private readonly BookRepository _bookRepository;
+
+        public BookService(BookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public async Task<BookDTO?> GetBookByPlanIdAsync(int planId)
+        {
+            var book = await _bookRepository.GetBookByPlanIdAsync(planId);
+
+            if (book == null) return null;
+
+            return new BookDTO
+            {
+                BookName = book.BookName,
+                Description = book.Description,
+                Link = book.Link
+            };
+        }
+
+        public async Task AddBookAsync(int planId, BookDTO bookDto)
+        {
+            var book = new Book
+            {
+                BookName = bookDto.BookName,
+                Description = bookDto.Description,
+                Link = bookDto.Link
+            };
+
+            await _bookRepository.AddBookAsync(planId, book);
+        }
+    }
+}
---
+++ Service/PostService.cs
@@ -0,0 +1,57 @@
+﻿using OnlineBookClub.DTO;
+using OnlineBookClub.Models;
+using OnlineBookClub.Repository;
+
+namespace OnlineBookClub.Service
+{
+    public class PostService 
+    {
+        private readonly PostRepository _repository;
+
+        public PostService(PostRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Post> CreatePostAsync(int userId, string userName, PostDTO dto)
+        {
+            var post = new Post
+            {
+                Plan_Id = dto.PlanId,
+                User_Id = userId,
+                Name = userName,
+                Content = dto.Content,
+                Img_Path = dto.ImgPath ?? "",
+                CreateTime = DateTime.UtcNow
+            };
+
+            return await _repository.CreatePostAsync(post);
+        }
+
+        public async Task<IEnumerable<Post>> GetPostsByPlanIdAsync(int planId)
+        {
+            return await _repository.GetPostsByPlanIdAsync(planId);
+        }
+
+        public async Task<bool> UpdatePostAsync(int postId, int userId, PostDTO dto)
+        {
+            var post = await _repository.GetPostByIdAsync(postId);
+            if (post == null || post.User_Id != userId)
+                return false;
+
+            post.Content = dto.Content;
+            post.Img_Path = dto.ImgPath ?? "";
+
+            return await _repository.UpdatePostAsync(post);
+        }
+
+        public async Task<bool> DeletePostAsync(int postId, int userId)
+        {
+            var post = await _repository.GetPostByIdAsync(postId);
+            if (post == null || post.User_Id != userId)
+                return false;
+
+            return await _repository.DeletePostAsync(postId);
+        }
+    }
+}

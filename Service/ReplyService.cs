2e107041b091748a57b87bdead03e71cca0583fe
Stuvian <hp40402@gmail.com>
2025-04-28 16:03:58 +0800

    回復 img路徑修改

--- Service/ReplyService.cs
+++ Service/ReplyService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Azure.Core;
+using Microsoft.Extensions.Hosting;
 using OnlineBookClub.DTO;
 using OnlineBookClub.Models;
 using OnlineBookClub.Repository;
@@ -105,18 +106,18 @@ public class ReplyService
 
         return await _repository.DeleteReplyAsync(replyId);
     }
-    public async Task<ReplyDTO?> GetReplyByIdAsync(string email, int ReplyID)
+    public async Task<ReplyDTO?> GetReplyByIdAsync(string email, int ReplyID, HttpRequest request)
     {
         var Reply = await _repository.GeReplyByIdWithUserAsync(ReplyID);
         if (Reply == null) return null;
-
+        var hostUrl = $"{request.Scheme}://{request.Host}"; // ex: https://localhost:7009
         return new ReplyDTO
         {
             
             PostId = Reply.Post_Id,
             ReplyId= Reply.Reply_Id,
             ReplyContent = Reply.ReplyContent,
-            ImgPath = Reply.ReplyImg,
+            ImgPath = string.IsNullOrEmpty(Reply.ReplyImg) ? null : $"{hostUrl}{Reply.ReplyImg}",
             CreateTime = Reply.ReplyTime,
             MemberPath = Reply.User?.ProfilePictureUrl, // 改成拿發文者的資料
             Name = Reply.User?.UserName
@@ -125,16 +126,17 @@ public class ReplyService
 
 
 
-    public async Task<IEnumerable<ReplyDTO>> GetReplyByUserIdAsync(int userId, string email)
+    public async Task<IEnumerable<ReplyDTO>> GetReplyByUserIdAsync(int userId, string email, HttpRequest request)
     {
         var Replies = await _repository.GetReplyByUserIdAsync(userId);
         var data =_membersRepository.getbyid(userId);
+        var hostUrl = $"{request.Scheme}://{request.Host}"; // ex: https://localhost:7009
         return Replies.Select(Reply => new ReplyDTO
         {
             ReplyId = Reply.Reply_Id,
             PostId = Reply.Post_Id,
             ReplyContent = Reply.ReplyContent,
-            ImgPath = Reply.ReplyImg,
+            ImgPath = string.IsNullOrEmpty(Reply.ReplyImg) ? null : $"{hostUrl}{Reply.ReplyImg}",
             CreateTime = Reply.ReplyTime,
             MemberPath = data.ProfilePictureUrl,
             Name = data.UserName
